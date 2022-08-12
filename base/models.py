fffeb5c476f0e26c2cce3399b01a2c1bf54503a5
ShivamMalpani <shivammalpani@iitbhilai.ac.in>
2022-08-12 14:57:06 +0530

    create operation

--- base/models.py
+++ base/models.py
@@ -1,16 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 # Create your models here.
+class Topic(models.Model):
+    name = models.TextField(max_length=50)
 
+    def __str__(self):
+        return self.name
 class Room(models.Model):
-    # host =
-    #topic =
-    # id = 132
+    host = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    topic = models.ForeignKey(Topic,on_delete=models.SET_NULL,null=True)
     name = models.CharField(max_length=20)
     description = models.TextField(null=True, blank=True) # database can have this value empty
     # participants =
     updated = models.DateTimeField(auto_now=True) # value will change whenever we save
     created = models.DateTimeField(auto_now_add=True) # value wont change anytime. It saves value only when its is first time saved
 
+    class Meta:
+        ordering = ['-updated','-created']
     def __str__(self):
         return str(self.name)
+
+# class Message(models.Model):
+#     user = models.ForeignKey(User,on_delete=models.CASCADE)
+#     # room = models.ForeignKey(User,on_delete=models.CASCADE)
+#     body = models.TextField()
+#     update = models.DateTimeField(auto_now=True)
+#     created = models.DateTimeField(auto_now_add=True)
+#     def __str__(self):
+#         return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 from django.http import HttpResponse
 from .models import Room
-
+from .forms import RoomForm
 # rooms = [
 #     {'id':1,'name':'Be consistent'},
 #     {'id':2, 'name': 'Be productive'},
@@ -11,9 +11,9 @@ from .models import Room
 
 def home(request):
     rooms = Room.objects.all()
-    for i in rooms:
-        print(i)
-    context = { 'rooms':rooms}
+    # for i in rooms:
+    #     room = i
+    context = {'room':rooms}
     return render(request,'base/home.html',context)
 
 # rooms = [{'id':3, 'name':'Frontend'},{'id':2,'name':'Backend'},{'id':1,'name':'DSA'}]
@@ -31,12 +31,24 @@ def room(request, pk):
     return render(request,'Room.html',context)
 
 def createRoom(request):
-    room = Room.objects.all()
-    context={'room':room}
+    # room = Room.objects.all()
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context={'form': form}
     return render(request,'base/create_room.html',context)
 
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     print(room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('base/home.html')
     context = {'room': room}
     return render(request,'base/update.html',context)
